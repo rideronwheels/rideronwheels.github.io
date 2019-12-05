7b9202b9fa0987bdc092beeeff3e423cfc32e08d
Niyas C <niyasmonc@gmail.com>
2019-12-05 22:48:37 +0530

    new about and vidoes

--- assets/script/app.js
+++ assets/script/app.js
@@ -23,7 +23,17 @@ $(document).ready(function() {
     });
 });
 
-
+    /*--/ Star Typed /--*/
+    if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+        var typed = new Typed('.text-slider', {
+            strings: typed_strings.split(','),
+            typeSpeed: 80,
+            loop: true,
+            backDelay: 1100,
+            backSpeed: 30
+        });
+    }
 $(function() {
     $('.nav-item').on('click', function() {
         $('.nav-item').removeClass('active');
