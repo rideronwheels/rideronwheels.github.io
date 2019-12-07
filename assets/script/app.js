447169488b02e3b42972ff9aae71dc9551e0f204
Niyas C <niyasmonc@gmail.com>
2019-12-07 22:26:44 +0800

    Correct the handling of navbar collapse logic

--- assets/script/app.js
+++ assets/script/app.js
@@ -38,7 +38,7 @@ $(function() {
     $('.nav-item').on('click', function() {
         $('.nav-item').removeClass('active');
         $(this).addClass('active');
+        // Collapse navbar
+        $('.navbar-toggler').click();
     });
-    // Collapse navbar
-    $('.navbar-toggler').click();
 });
